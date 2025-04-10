33a0f772aaf50b282b8f99eba14deea05da9f712
alsdk1676 <alsdk6761@gmail.com>
2025-04-10 17:48:48 +0900

    controller day01

--- src/main/java/com/app/controller/controller/ExampleController.java
+++ src/main/java/com/app/controller/controller/ExampleController.java
@@ -1,19 +1,23 @@
 package com.app.controller.controller;
 
+import com.app.controller.domain.ProductVO;
+import com.app.controller.domain.UserVO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/ex/*")
+@RequestMapping("/ex/*") // 폴더명
 @Slf4j
 public class ExampleController {
 
 //  localhost:10000/ex/ex01
-    @GetMapping("ex01")
+    @GetMapping("ex01") // 파일명
     public void ex01() {
         log.info("ex01 여기 실행됨!");
     };
@@ -26,15 +30,39 @@ public class ExampleController {
 //        log.info(name);
 //    }
 
+//    @GetMapping("ex02")
+//    public void ex02(String name) {
+//        log.info(name);
+//    }
+
+    // 방법1
+//    @GetMapping("ex02")
+//    public void ex02(String name, Model model) {
+//        log.info(name);
+//        model.addAttribute("name", name); // add : 화면으로 보냄
+//    }
+//
+//
+     // 방법2
     @GetMapping("ex02")
-    public void ex02(String name) {
+    public void ex02(@ModelAttribute("name") String name) {
         log.info(name);
     }
 
     // ex03을 만들고 나이를 전달받기
     // 이름과 나이 둘 다 받아서 로그 출력
+
+    // 이름과 나이를 화면으로 보내서, 이름, 나이, 만나이(나이 - 1)를 출력하기
+//    @GetMapping("ex03")
+//    public void ex03(String name, int age) {
+//        log.info(name);
+//        log.info(String.valueOf(age));
+////        log.info("{}", age));
+////        log.info("" + age);
+//    }
+
     @GetMapping("ex03")
-    public void ex03(String name, int age) {
+    public void ex03(@ModelAttribute("name") String name, @ModelAttribute("age") int age) {
         log.info(name);
         log.info(String.valueOf(age));
 //        log.info("{}", age));
@@ -45,10 +73,28 @@ public class ExampleController {
 //  ex04
 //  직업, 이름
     @GetMapping("ex04")
-    public void ex04(@RequestParam("job") String job, @RequestParam("name") String name) {
+    public void ex04(@RequestParam("job") String job, @RequestParam("name") String name, Model model) {
+        model.addAttribute("job", job);
+        model.addAttribute("name", name);
         log.info(job);
         log.info(name);
     }
 
+//    ex05
+//    화면에서 UserVO 받기
+    @GetMapping("ex05")
+    // 필드값의 이름과 일치하면 key와 필드를 UserVO에 바로 매칭해서 넣어줌
+    public void ex05(UserVO userVO) {
+        log.info(userVO.toString());
+    }
 
+//    ex06
+//  상품이름(productName) 상품가격(productPrice)
+//  productVO로 생성한 후
+//  화면에서 ex06으로 상품명과 상품가격을 받아서 요청했을 때
+//  log로 productVO 출력하기!
+    @GetMapping("ex06")
+    public void ex06(@ModelAttribute ProductVO productVO) { // @로 화면으로 바로 보내기?
+        log.info(productVO.toString());
+    }
 }
